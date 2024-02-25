cdfd938003eec1e19d3bbb850f40e803c9194cdb
josuehoenicka <josuee.vzla@gmail.com>
2024-02-25 08:33:31 -0300

    feat: learning arithmetic

--- 1 - Variables/index.php
+++ 1 - Variables/index.php
@@ -1,7 +1,14 @@
 <?php
     $name = "Josue";
     $year = 2001;
+    $empty = false;
 
     echo "Hi, my name is {$name} <br/>";
-    echo "I was born in {$year}";
+    echo "I was born in {$year} <br/>";
+    echo "Text empty:" . $empty . "<br/>";
+
+    $price = 6.99;
+    $quantity = 30;
+    $total = $price * $quantity;
+    echo "Total is: \${$total}"
 ?>
---
+++ 2- arithmetic/index.php
@@ -0,0 +1,25 @@
+<?php
+$x = 10;
+$z = 2;
+$y = 43;
+$w = 12;
+$counter = 1;
+
+echo $x + $z;
+echo "<br/>";
+
+echo $x - $z;
+echo "<br/>";
+
+echo $y * $w;
+echo "<br/>";
+
+echo $y / $w;
+echo "<br/>";
+
+echo $counter;
+echo "<br/>";
+
+$counter++;
+echo $counter;
+?>
