ab0df63748b5f32c61c02f0ae71900d706ae7d71
josuehoenicka <josuee.vzla@gmail.com>
2024-02-25 20:34:33 -0300

    feat: learning switches

---
+++ 7 - switches/index.php
@@ -0,0 +1,16 @@
+<?php
+$option = 2;
+
+switch ($option) {
+    case 1:
+        echo "Option is 1";
+        break;
+    case 2:
+        echo "Option is 2";
+        break;
+    case 3:
+        echo "Option is 3";
+        break;
+    default:
+        echo "Invalid option";
+}
