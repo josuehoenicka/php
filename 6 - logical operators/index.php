3c491a17da68c53adf16a82287c93ba774971fa5
josuehoenicka <josuee.vzla@gmail.com>
2024-02-25 20:15:58 -0300

    feat: learning logical operators

---
+++ 6 - logical operators/index.php
@@ -0,0 +1,16 @@
+<?php
+
+$case1 = false;
+$case2 = true;
+
+if ($case1 && $case2){
+ echo "if (case1 && case2) actived <br/>";
+}
+
+if ($case1 || $case2){
+    echo "if (case1 || case2) actived <br/>";
+}
+
+if(!$case1){
+    echo "if(!case1) actived <br/>";
+}
